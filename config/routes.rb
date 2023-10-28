58bb06155e9f891c32861fce2fcdb7078e70cc27
Haruka Murakami <2111140025k@ed.fuk.kindai.ac.jp>
2023-10-28 14:57:02 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
+  # get 'tweets/index'
+  # get 'tweets/new'
+  # get 'tweets/create'
+  # get 'tweets/destroy'
   get 'users/index'
   get 'users/new'
   get 'users/create'
   get 'users/destroy'
   post 'users/create'
-  #resources :users
-  root 'users#index'
-  delete 'users/:id', to: 'users#destroy'
-  #get 'users/:id', to: 'users#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  #root 'users#index'
+  resources :tweets
+  root 'tweets#index'
+  delete 'users/:id', to: 'users#destroy'
+  post 'top/login'
+  get 'get/logout'
   # Defines the root path route ("/")
   # root "articles#index"
 end
